89ba862f5361f35aeb62b9013fb128cbbc599d4f
welasco <victor@hepoca.com>
2022-08-25 11:35:09 -0500

    Fixing log messages and renaming backup files

--- AzureVMSSLBUpgradeModule/modules/BackupBasicLoadBalancer/BackupBasicLoadBalancer.psm1
+++ AzureVMSSLBUpgradeModule/modules/BackupBasicLoadBalancer/BackupBasicLoadBalancer.psm1
@@ -5,9 +5,12 @@ function BackupBasicLoadBalancer {
         [Parameter(Mandatory = $True)][Microsoft.Azure.Commands.Network.Models.PSLoadBalancer] $BasicLoadBalancer
     )
     log -Message "[BackupBasicLoadBalancer] Initiating Backup of Basic Load Balancer Configurations"
-    ConvertTo-Json -Depth 100 $BasicLoadBalancer | Out-File -FilePath ($BasicLoadBalancer.Name + "-" + (Get-Date -Format FileDateTime) + ".json")
+    $backupDateTime = Get-Date -Format FileDateTime
+    ConvertTo-Json -Depth 100 $BasicLoadBalancer | Out-File -FilePath ($BasicLoadBalancer.Name + "-" + $backupDateTime + ".json")
+    log -Message "[BackupBasicLoadBalancer] JSON backup Basic Load Balancer ($BasicLoadBalancer.Name + "-" + $backupDateTime + ".json") Completed"
     Export-AzResourceGroup -ResourceGroupName $BasicLoadBalancer.ResourceGroupName -Resource $BasicLoadBalancer.Id -SkipAllParameterization  | Out-Null
-    Move-Item ($BasicLoadBalancer.ResourceGroupName + ".json") ($BasicLoadBalancer.ResourceGroupName + "-" + (Get-Date -Format FileDateTime) + ".json")
+    Move-Item ($BasicLoadBalancer.ResourceGroupName + ".json") ("ARM-" + $BasicLoadBalancer.Name + "-" + $backupDateTime + ".json")
+    log -Message "[BackupBasicLoadBalancer] ARM Template Backup Basic Load Balancer $("ARM-" + $BasicLoadBalancer.Name + "-" + $backupDateTime + ".json") Completed"
     log -Message "[BackupBasicLoadBalancer] End Backup of Basic Load Balancer Configurations"
 }
 Export-ModuleMember -Function BackupBasicLoadBalancer
