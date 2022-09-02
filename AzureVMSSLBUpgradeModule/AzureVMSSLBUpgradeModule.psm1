35db5bc63a6d7d43e49bca20e55ef3d01b9d92b5
Matthew Bratschun <matthew.bratschun@microsoft.com>
2022-09-02 11:16:22 -0600

    throw

--- AzureVMSSLBUpgradeModule/AzureVMSSLBUpgradeModule.psm1
+++ AzureVMSSLBUpgradeModule/AzureVMSSLBUpgradeModule.psm1
@@ -1,17 +1,16 @@
 # verify that required Az modules are available
 
 If (!(Get-Module -Name Az.Accounts -ListAvailable)) {
-    Write-Error "The 'Az.Accounts' PowerShell module is not installed on this system. To install the required modules for this script, run 'Install-Module -Name Az.Accounts,Az.Compute,Az.Network,Az.Resources'" -ErrorAction Stop
+    throw "The 'Az.Accounts' PowerShell module is not installed on this system. To install the required modules for this script, run 'Install-Module -Name Az.Accounts,Az.Compute,Az.Network,Az.Resources'"
 }
 If (!(Get-Module -Name Az.Compute -ListAvailable)) {
-    Write-Error "The 'Az.Compute' PowerShell module is not installed on this system. To install the required modules for this script, run 'Install-Module -Name Az.Accounts,Az.Compute,Az.Network,Az.Resources'" -ErrorAction Stop
-
+    throw "The 'Az.Compute' PowerShell module is not installed on this system. To install the required modules for this script, run 'Install-Module -Name Az.Accounts,Az.Compute,Az.Network,Az.Resources'"
 }
 If (!(Get-Module -Name Az.Network -ListAvailable)) {
-    Write-Error "The 'Az.Network' PowerShell module is not installed on this system. To install the required modules for this script, run 'Install-Module -Name Az.Accounts,Az.Compute,Az.Network,Az.Resources'" -ErrorAction Stop
+    throw "The 'Az.Network' PowerShell module is not installed on this system. To install the required modules for this script, run 'Install-Module -Name Az.Accounts,Az.Compute,Az.Network,Az.Resources'"
 }
 If (!(Get-Module -Name Az.Resources -ListAvailable)) {
-    Write-Error "The 'Az.Resources' PowerShell module is not installed on this system. To install the required modules for this script, run 'Install-Module -Name Az.Accounts,Az.Compute,Az.Network,Az.Resources'" -ErrorAction Stop
+    throw "The 'Az.Resources' PowerShell module is not installed on this system. To install the required modules for this script, run 'Install-Module -Name Az.Accounts,Az.Compute,Az.Network,Az.Resources'"
 }
 
 Import-Module $PSScriptRoot\modules\AzureVMSSLBUpgrade\AzureVMSSLBUpgrade.psd1
