09653e6d90007a3d5a9dfa8b0bd4151201f84618
welasco <victor@hepoca.com>
2022-09-02 08:14:19 -0500

    Add scenarios module to clean the code

--- AzureVMSSLBUpgradeModule/modules/AzureVMSSLBUpgrade/AzureVMSSLBUpgrade.psm1
+++ AzureVMSSLBUpgradeModule/modules/AzureVMSSLBUpgrade/AzureVMSSLBUpgrade.psm1
@@ -40,18 +40,8 @@
 #>
 # Load Modules
 Import-Module ((Split-Path $PSScriptRoot -Parent)+"\Log\Log.psd1")
-Import-Module ((Split-Path $PSScriptRoot -Parent)+"\BackupBasicLoadBalancer\BackupBasicLoadBalancer.psd1")
-Import-Module ((Split-Path $PSScriptRoot -Parent)+"\PublicFEMigration\PublicFEMigration.psd1")
-Import-Module ((Split-Path $PSScriptRoot -Parent)+"\RemoveLBFromVMSS\RemoveLBFromVMSS.psd1")
-Import-Module ((Split-Path $PSScriptRoot -Parent)+"\BackendPoolMigration\BackendPoolMigration.psd1")
-Import-Module ((Split-Path $PSScriptRoot -Parent)+"\NatRulesMigration\NatRulesMigration.psd1")
-Import-Module ((Split-Path $PSScriptRoot -Parent)+"\InboundNatPoolsMigration\InboundNatPoolsMigration.psd1")
-Import-Module ((Split-Path $PSScriptRoot -Parent)+"\ProbesMigration\ProbesMigration.psd1")
-Import-Module ((Split-Path $PSScriptRoot -Parent)+"\LoadBalacingRulesMigration\LoadBalacingRulesMigration.psd1")
-Import-Module ((Split-Path $PSScriptRoot -Parent)+"\OutboundRulesCreation\OutboundRulesCreation.psd1")
-Import-Module ((Split-Path $PSScriptRoot -Parent)+"\NSGCreation\NSGCreation.psd1")
 Import-Module ((Split-Path $PSScriptRoot -Parent)+"\ValidateScenario\ValidateScenario.psd1")
-Import-Module ((Split-Path $PSScriptRoot -Parent)+"\PrivateFEMigration\PrivateFEMigration.psd1")
+Import-Module ((Split-Path $PSScriptRoot -Parent)+"\ScenariosMigration\ScenariosMigration.psd1")
 
 function AzureVMSSLBUpgrade {
     Param(
@@ -93,72 +83,15 @@ function AzureVMSSLBUpgrade {
     # verify basic load balancer configuration is a supported scenario
     $scenario = Test-SupportedMigrationScenario -BasicLoadBalancer $BasicLoadBalancer -StdLoadBalancer $StandardLoadBalancerName
 
-    #$vmssNames = $BasicLoadBalancer.BackendAddressPools.BackendIpConfigurations.Id | Where-Object{$_ -match "Microsoft.Compute/virtualMachineScaleSets"} | ForEach-Object{$_.split("/")[8]} | Select-Object -Unique
-    #$vmssIds = $BasicLoadBalancer.BackendAddressPools.BackendIpConfigurations.Id | Where-Object{$_ -match "Microsoft.Compute/virtualMachineScaleSets"} | Select-Object -Unique
-    $vmssIds = $BasicLoadBalancer.BackendAddressPools.BackendIpConfigurations.id | Foreach-Object{$_.split("virtualMachines")[0]} | Select-Object -Unique
-
-    # Backup Basic Load Balancer Configurations
-    BackupBasicLoadBalancer -BasicLoadBalancer $BasicLoadBalancer
-
-    # Deletion of Basic Load Balancer and Delete Basic Load Balancer
-    RemoveLBFromVMSS -vmssIds $vmssIds -BasicLoadBalancer $BasicLoadBalancer
-
-    # Creation of Standard Load Balancer
-    $StdLoadBalancerName = ($PSBoundParameters.ContainsKey("StandardLoadBalancerName")) ? $StandardLoadBalancerName : $BasicLoadBalancer.Name
-    $StdLoadBalancerDef = @{
-        ResourceGroupName = $BasicLoadBalancer.ResourceGroupName
-        Name = $StdLoadBalancerName
-        SKU = "Standard"
-        location = $BasicLoadBalancer.Location
-    }
-    try {
-        $ErrorActionPreference = 'Stop'
-        $StdLoadBalancer = New-AzLoadBalancer @StdLoadBalancerDef
-    }
-    catch {
-        $message = @"
-            [AzureVMSSLBUpgrade] An error occured when creating the new Standard load balancer '$StdLoadBalancerName'. To recover,
-            redeploy the Basic load balancer from the 'ARMTemplate-$($BasicLoadBalancer.Name)-ResourceGroupName...'
-            file, re-add the original backend pool members (see file 'State-$($BasicLoadBalancer.Name)-ResourceGroupName...'
-            BackendIpConfigurations), address the following error, and try again. Error message: $_
-"@
-        log 'Error' $message
-        Exit
-    }
-
     # Migration of Frontend IP Configurations
     switch ($scenario.ExternalOrInternal) {
         'internal' {
-            PrivateFEMigration -BasicLoadBalancer $BasicLoadBalancer -StdLoadBalancer $stdLoadBalancer
+            InternalLBMigration -BasicLoadBalancer $BasicLoadBalancer -StandardLoadBalancerName $StandardLoadBalancerName
         }
         'external' {
-            PublicFEMigration -BasicLoadBalancer $BasicLoadBalancer -StdLoadBalancer $StdLoadBalancer
+            PublicLBMigration -BasicLoadBalancer $BasicLoadBalancer -StandardLoadBalancerName $StandardLoadBalancerName
         }
     }
-
-    # Migration of Backend Address Pools
-    BackendPoolMigration -BasicLoadBalancer $BasicLoadBalancer -StdLoadBalancer $StdLoadBalancer
-
-    # Migration of NAT Rules
-    NatRulesMigration -BasicLoadBalancer $BasicLoadBalancer -StdLoadBalancer $StdLoadBalancer
-
-    # Migration of Inbound NAT Pools
-    InboundNatPoolsMigration -BasicLoadBalancer $BasicLoadBalancer -StdLoadBalancer $StdLoadBalancer
-
-    # Migration of Probes
-    ProbesMigration -BasicLoadBalancer $BasicLoadBalancer -StdLoadBalancer $StdLoadBalancer
-
-    # Migration of Load Balancing Rules
-        # *** Use default outbound access configuration **does default outbound use the standard LB public IP (matching the basic LB behavior)?
-        # We need to check if we will create an outbound rule for the Standard LB or not
-    LoadBalacingRulesMigration -BasicLoadBalancer $BasicLoadBalancer -StdLoadBalancer $StdLoadBalancer
-
-    # Creating Outbound Rules for SNAT
-    OutboundRulesCreation -StdLoadBalancer $StdLoadBalancer
-
-    # Creating NSG for Standard Load Balancer
-    NSGCreation -BasicLoadBalancer $BasicLoadBalancer -StdLoadBalancer $StdLoadBalancer
-
     log -Message "############################## Migration Completed ##############################"
 }
 
