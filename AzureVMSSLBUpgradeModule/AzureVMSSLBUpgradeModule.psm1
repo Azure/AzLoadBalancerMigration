76ac73a42d5a27a0761deb58d29e2792a6318ca1
Matthew Bratschun <matthew.bratschun@microsoft.com>
2022-09-02 11:50:57 -0600

    required version messages

--- AzureVMSSLBUpgradeModule/AzureVMSSLBUpgradeModule.psm1
+++ AzureVMSSLBUpgradeModule/AzureVMSSLBUpgradeModule.psm1
@@ -1,20 +1,25 @@
 # verify that required Az modules are available
+$requiredModules = @(
+    @{name = 'Az.Accounts'; requiredVersion = [Version]::new(2,9,0,0)}
+    @{name = 'Az.Compute'; requiredVersion = [Version]::new(4,30,0,0)}
+    @{name = 'Az.Network'; requiredVersion = [Version]::new(4,20,0,0)}
+    @{name = 'Az.Resources'; requiredVersion = [Version]::new(6,1,0,0)}
+)
 
-If (!(Get-Module -Name Az.Accounts -ListAvailable -Refresh)) {
-    Write-Error "The 'Az.Accounts' PowerShell module is not installed on this system. To install the required modules for this script, run 'Install-Module -Name Az.Accounts,Az.Compute,Az.Network,Az.Resources'"
-    return
-}
-If (!(Get-Module -Name Az.Compute -ListAvailable)) {
-    Write-Error "The 'Az.Compute' PowerShell module is not installed on this system. To install the required modules for this script, run 'Install-Module -Name Az.Accounts,Az.Compute,Az.Network,Az.Resources'"
-    return
-}
-If (!(Get-Module -Name Az.Network -ListAvailable)) {
-    Write-Error "The 'Az.Network' PowerShell module is not installed on this system. To install the required modules for this script, run 'Install-Module -Name Az.Accounts,Az.Compute,Az.Network,Az.Resources'"
-    return
-}
-If (!(Get-Module -Name Az.Resources -ListAvailable)) {
-    Write-Error "The 'Az.Resources' PowerShell module is not installed on this system. To install the required modules for this script, run 'Install-Module -Name Az.Accounts,Az.Compute,Az.Network,Az.Resources'"
-    return
+$installMessage = "The '{0}' PowerShell module is not installed on this system. To install the required modules for this script, run 'Install-Module -Name Az.Accounts,Az.Compute,Az.Network,Az.Resources'"
+$versionMessage = "The installed '{0}' PowerShell module version '{1}' is outdated; this script expects at least version '{2}'. To update the required modules for this script, run 'Update-Module -Name Az.Accounts,Az.Compute,Az.Network,Az.Resources'"
+
+ForEach ($requiredModule in $requiredModules) {
+    $module = Get-Module -Name $requiredModule.name -ListAvailable -Refresh
+
+    If ([string]::IsNullOrEmpty($module)) {
+        Write-Error ($installMessage -f $requiredModule.name)
+        return
+    }
+    ElseIf ($module.Version -lt $requiredModule.requiredVersion) {
+        Write-Error ($versionMessage -f $requiredModule.Name,$module.Version,$requiredModule.requiredVersion)
+        return
+    }
 }
 
 Import-Module $PSScriptRoot\modules\AzureVMSSLBUpgrade\AzureVMSSLBUpgrade.psd1
