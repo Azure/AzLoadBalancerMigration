6c464bfe567f6a70cad88f16bdccfe8b85939c3f
welasco <victor@hepoca.com>
2022-09-06 16:19:50 -0500

    Removing duplicated backup files

--- AzureVMSSLBUpgradeModule/modules/BackupBasicLoadBalancer/BackupBasicLoadBalancer.psm1
+++ AzureVMSSLBUpgradeModule/modules/BackupBasicLoadBalancer/BackupBasicLoadBalancer.psm1
@@ -5,23 +5,23 @@ function BackupBasicLoadBalancer {
         [Parameter(Mandatory = $True)][Microsoft.Azure.Commands.Network.Models.PSLoadBalancer] $BasicLoadBalancer
     )
     log -Message "[BackupBasicLoadBalancer] Initiating Backup of Basic Load Balancer Configurations"
-    $backupDateTime = Get-Date -Format FileDateTime
-    $outputFileName = ('State-' + $BasicLoadBalancer.Name + "-" + $BasicLoadBalancer.ResourceGroupName + "-" + $backupDateTime + ".json")
-    ConvertTo-Json -Depth 100 $BasicLoadBalancer | Out-File -FilePath $outputFileName
-    log -Message "[BackupBasicLoadBalancer] JSON backup Basic Load Balancer to file $outputFileName Completed"
-
     try {
         $ErrorActionPreference = 'Stop'
+
+        $backupDateTime = Get-Date -Format FileDateTime
+        $outputFileName = ('State-' + $BasicLoadBalancer.Name + "-" + $BasicLoadBalancer.ResourceGroupName + "-" + $backupDateTime + ".json")
+        ConvertTo-Json -Depth 100 $BasicLoadBalancer | Out-File -FilePath $outputFileName
+        log -Message "[BackupBasicLoadBalancer] JSON backup Basic Load Balancer to file $outputFileName Completed"
+
         Export-AzResourceGroup -ResourceGroupName $BasicLoadBalancer.ResourceGroupName -Resource $BasicLoadBalancer.Id -SkipAllParameterization > $null
+        $newExportedResourceFileName = ("ARMTemplate-" + $BasicLoadBalancer.Name + "-" + $BasicLoadBalancer.ResourceGroupName + '-' + $backupDateTime + ".json")
+        Move-Item ($BasicLoadBalancer.ResourceGroupName + ".json") $newExportedResourceFileName
     }
     catch {
         $message = "[BackupBasicLoadBalancer] An error occured while exporting the basic load balancer '$($BasicLoadBalancer.Name)' to an ARM template for backup purposes. Error: $_"
         log -Severity Error -Message $message
         Exit
     }
-
-    $newExportedResourceFileName = ("ARMTemplate-" + $BasicLoadBalancer.Name + "-" + $BasicLoadBalancer.ResourceGroupName + '-' + $backupDateTime + ".json")
-    Move-Item ($BasicLoadBalancer.ResourceGroupName + ".json") $newExportedResourceFileName
     log -Message "[BackupBasicLoadBalancer] ARM Template Backup Basic Load Balancer to file $($newExportedResourceFileName) Completed"
 }
 Export-ModuleMember -Function BackupBasicLoadBalancer
