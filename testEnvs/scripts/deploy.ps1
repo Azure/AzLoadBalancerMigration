a3d20c82b909a82a36ef0a3612e3fbb53343c37e
Matthew Bratschun <matthew.bratschun@microsoft.com>
2022-09-06 13:30:26 -0600

    import for multiple modules

--- testEnvs/scripts/deploy.ps1
+++ testEnvs/scripts/deploy.ps1
@@ -46,7 +46,7 @@ if ($Cleanup -and $null -ne $filteredTemplates) {
         $jobs += $(Remove-AzResourceGroup -Name "rg-$($_.BaseName)" -Force -AsJob)
     }
 
-    $jobs | Wait-Job | Receive-Job
+    $jobs | Wait-Job 
     return
 }
 
