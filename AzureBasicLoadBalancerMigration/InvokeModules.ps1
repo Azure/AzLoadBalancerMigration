09e78d8afc41d926f494ee00e90871458783cdfb
Matthew Bratschun <matthew.bratschun@microsoft.com>
2022-09-16 15:45:19 -0600

    module dir rename

---
+++ AzureBasicLoadBalancerMigration/InvokeModules.ps1
@@ -0,0 +1,70 @@
+Remove-Module AzureBasicLoadBalancerMigration -force
+Import-Module C:\Projects\VSProjects\VMSS-LoadBalander-MIgration\AzureBasicLoadBalancerMigration\AzureBasicLoadBalancerMigration.psd1 -Force
+
+# External
+#Start-AzBasicLoadBalancerMigration -ResourceGroupName basiclb -BasicLoadBalancerName basiclb-loadbalancer -StandardLoadBalancerName stdlb-loadbalancer
+# Start-AzBasicLoadBalancerMigration -ResourceGroupName basiclb -BasicLoadBalancerName basiclb-loadbalancer
+#Start-AzBasicLoadBalancerMigration -RestoreFromJsonFile State-BasicLB-LoadBalancer-basiclb-20220907T1752322840.json
+#Start-AzBasicLoadBalancerMigration -ResourceGroupName "rg-009-basic-lb-ext-basic-static-pip" -BasicLoadBalancerName "lb-basic-01"
+
+# Internal
+#Start-AzBasicLoadBalancerMigration -ResourceGroupName BasicLBInternal -BasicLoadBalancerName BasicLBInternal-LoadBalancer
+Start-AzBasicLoadBalancerMigration -ResourceGroupName rg-013-vmss-multi-be-single-lb -BasicLoadBalancerName lb-basic-01
+
+# Test IPV6
+# Start-AzBasicLoadBalancerMigration -ResourceGroupName rg-012-basic-lb-ext-ipv6-fe -BasicLoadBalancerName lb-basi-c01
+
+# Test with piping the object
+#$lb = Get-AzLoadBalancer -ResourceGroupName "rg-009-basic-lb-ext-basic-static-pip" -Name "lb-basic-01"
+#$lb | Start-AzBasicLoadBalancerMigration
+
+# Testing passing an object
+#$lb = Get-AzLoadBalancer -ResourceGroupName "rg-009-basic-lb-ext-basic-static-pip" -Name "lb-basic-01"
+#Start-AzBasicLoadBalancerMigration -BasicLoadBalancer $lb
+
+#Start-AzBasicLoadBalancerMigration -ResourceGroupName basiclb2 -BasicLoadBalancerName basiclb2-loadbalancer -StandardLoadBalancerName stdlb2-loadbalancer
+
+############################################
+# Build test environment
+# Remove-AzResourceGroup -Name rg-009-basic-lb-ext-basic-static-pip -Force
+# cd testEnvs\scripts
+#.\deploy.ps1 -Location CentralUS -KeyVaultResourceGroupName rg-vmsstestingconfig -ScenarioNumber 012
+############################################
+
+#############
+# Test Matrix
+#############
+
+# Tested OK
+#Start-AzBasicLoadBalancerMigration -ResourceGroupName rg-001-basic-lb-int-single-fe -BasicLoadBalancerName lb-basic-01
+#Start-AzBasicLoadBalancerMigration -FailedMigrationRetryFilePath C:\Projects\VSProjects\VMSS-LoadBalander-MIgration\State-lb-basic-01-rg-001-basic-lb-int-single-fe-20220908T1530071833.json
+
+# Tested OK
+#Start-AzBasicLoadBalancerMigration -ResourceGroupName rg-002-basic-lb-int-multi-fe -BasicLoadBalancerName lb-basic-01
+
+# Tested OK
+#Start-AzBasicLoadBalancerMigration -ResourceGroupName rg-003-basic-lb-ext-single-fe -BasicLoadBalancerName lb-basic-01
+
+# Tested OK
+#Start-AzBasicLoadBalancerMigration -ResourceGroupName rg-004-basic-lb-ext-multi-fe -BasicLoadBalancerName lb-basic-01
+
+# Tested OK
+#Start-AzBasicLoadBalancerMigration -ResourceGroupName rg-005-basic-lb-int-single-be -BasicLoadBalancerName lb-basic-01
+
+# Tested OK
+#Start-AzBasicLoadBalancerMigration -ResourceGroupName rg-006-basic-lb-int-multi-be -BasicLoadBalancerName lb-basic-01
+
+# Tested Failed error in the scenario - already fixed and waiting deployment
+# This scenario is failing but I created it manualy without bicep and worked fine
+# Carml is causing issue here in the natrule
+#Start-AzBasicLoadBalancerMigration -ResourceGroupName rg-007-basic-lb-int-nat-rule -BasicLoadBalancerName lb-basic-01
+
+# did not test yet
+#Start-AzBasicLoadBalancerMigration -ResourceGroupName rg-009-basic-lb-ext-basic-static-pip -BasicLoadBalancerName lb-basic-01
+
+
+#############
+# End Test Matrix
+#############
+
+
---
+++ AzureBasicLoadBalancerMigration/modules/NSGCreation/NSGCreation.psm1
@@ -0,0 +1,136 @@
+# Load Modules
+Import-Module ((Split-Path $PSScriptRoot -Parent) + "\Log\Log.psd1")
+Import-Module ((Split-Path $PSScriptRoot -Parent) + "\UpdateVmssInstances\UpdateVmssInstances.psd1")
+function NSGCreation {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $True)][Microsoft.Azure.Commands.Network.Models.PSLoadBalancer] $BasicLoadBalancer,
+        [Parameter(Mandatory = $True)][Microsoft.Azure.Commands.Network.Models.PSLoadBalancer] $StdLoadBalancer
+    )
+    log -Message "[NSGCreation] Initiating NSG Creation"
+
+    log -Message "[NSGCreation] Looping all VMSS in the backend pool of the Load Balancer"
+    $vmssIds = $BasicLoadBalancer.BackendAddressPools.BackendIpConfigurations.id | Foreach-Object { $_.split("virtualMachines")[0] } | Select-Object -Unique
+    foreach ($vmssId in $vmssIds) {
+        $vmssName = $vmssId.split("/")[8]
+        $vmssRg = $vmssId.Split('/')[4]
+        $vmss = Get-AzVmss -ResourceGroupName $vmssRg -VMScaleSetName $vmssName
+
+        # Check if VMSS already has a NSG
+        log -Message "[NSGCreation] Checking if VMSS Named: $($vmss.Name) has a NSG"
+        if (![string]::IsNullOrEmpty($vmss.VirtualMachineProfile.NetworkProfile.NetworkInterfaceConfigurations.NetworkSecurityGroup)) {
+            log -Message "[NSGCreation] NSG detected in VMSS Named: $($vmss.Name) NetworkInterfaceConfigurations.NetworkSecurityGroup Id: $($vmss.VirtualMachineProfile.NetworkProfile.NetworkInterfaceConfigurations.NetworkSecurityGroup.Id)" -severity "Warning"
+            log -Message "[NSGCreation] NSG will not be created for VMSS Named: $($vmss.Name)" -severity "Warning"
+            break
+        }
+        log -Message "[NSGCreation] NSG not detected."
+
+        log -Message "[NSGCreation] Creating NSG for VMSS: $vmssName"
+
+        try {
+            $ErrorActionPreference = 'Stop'
+            $nsg = New-AzNetworkSecurityGroup -ResourceGroupName $vmssRg -Name ("NSG-" + $vmss.Name) -Location $vmss.Location -Force
+        }
+        catch {
+            $message = @"
+            [NSGCreation] An error occured while creating NSG '$("NSG-"+$vmss.Name)'. TRAFFIC FROM LOAD BALANCER TO BACKEND POOL MEMBERS WILL
+            BE BLOCKED UNTIL AN NSG WITH AN ALLOW RULE IS CREATED! To recover, manually create an NSG which allows traffic to the
+            backend ports on the VM/VMSS and associate it with the VM, VMSS, or subnet. Error: $_
+"@
+            log 'Error' $message
+            Exit
+        }
+
+        log -Message "[NSGCreation] NSG Named: $("NSG-"+$vmss.Name) created."
+
+        # Adding NSG Rule for each Load Balancing Rule
+        # Note: For now I'm assuming there is no way to have more than one VMSS in a single LB
+        log -Message "[NSGCreation] Adding one NSG Rule for each Load Balancing Rule"
+        $loadBalancingRules = $BasicLoadBalancer.LoadBalancingRules
+        $priorityCount = 100
+        foreach ($loadBalancingRule in $loadBalancingRules) {
+            $networkSecurityRuleConfig = @{
+                Name                                = ($loadBalancingRule.Name + "-loadBalancingRule")
+                Protocol                            = $loadBalancingRule.Protocol
+                SourcePortRange                     = "*"
+                DestinationPortRange                = $loadBalancingRule.BackendPort
+                SourceAddressPrefix                 = "*"
+                DestinationAddressPrefix            = "*"
+                SourceApplicationSecurityGroup      = $null
+                DestinationApplicationSecurityGroup = $null
+                Access                              = "Allow"
+                Priority                            = $priorityCount
+                Direction                           = "Inbound"
+            }
+            log -Message "[NSGCreation] Adding NSG Rule Named: $($networkSecurityRuleConfig.Name) to NSG Named: $($nsg.Name)"
+            $nsg | Add-AzNetworkSecurityRuleConfig @networkSecurityRuleConfig > $null
+            $priorityCount++
+        }
+
+        # Adding NSG Rule for each inboundNAT Rule
+        log -Message "[NSGCreation] Adding one NSG Rule for each inboundNatRule"
+        $networkSecurityRuleConfig = $null
+        $inboundNatRules = $BasicLoadBalancer.InboundNatRules
+        foreach ($inboundNatRule in $inboundNatRules) {
+            $networkSecurityRuleConfig = @{
+                Name                                = ($inboundNatRule.Name + "-NatRule")
+                Protocol                            = $inboundNatRule.Protocol
+                SourcePortRange                     = "*"
+                DestinationPortRange                = (($inboundNatRule.FrontendPortRangeStart).ToString() + "-" + ($inboundNatRule.FrontendPortRangeEnd).ToString())
+                SourceAddressPrefix                 = "*"
+                DestinationAddressPrefix            = "*"
+                SourceApplicationSecurityGroup      = $null
+                DestinationApplicationSecurityGroup = $null
+                Access                              = "Allow"
+                Priority                            = $priorityCount
+                Direction                           = "Inbound"
+            }
+            log -Message "[NSGCreation] Adding NSG Rule Named: $($networkSecurityRuleConfig.Name) to NSG Named: $($nsg.Name)"
+            $nsg | Add-AzNetworkSecurityRuleConfig @networkSecurityRuleConfig > $null
+            $priorityCount++
+        }
+
+        # Saving NSG
+        log -Message "[NSGCreation] Saving NSG Named: $($nsg.Name)"
+        try {
+            $ErrorActionPreference = 'Stop'
+            Set-AzNetworkSecurityGroup -NetworkSecurityGroup $nsg > $null
+        }
+        catch {
+            $message = @"
+            [NSGCreation] An error occured while adding security rules to NSG '$("NSG-"+$vmss.Name)'. TRAFFIC FROM LOAD BALANCER TO BACKEND POOL MEMBERS WILL
+            BE BLOCKED UNTIL AN NSG WITH AN ALLOW RULE IS CREATED! To recover, manually rules in NSG '$("NSG-"+$vmss.Name)' which allows traffic
+            to the backend ports on the VM/VMSS and associate the NSG with the VM, VMSS, or subnet. Error: $_
+"@
+            log 'Error' $message
+            Exit
+        }
+
+        # Adding NSG to VMSS
+        log -Message "[NSGCreation] Adding NSG Named: $($nsg.Name) to VMSS Named: $($vmss.Name)"
+        foreach ($networkInterfaceConfiguration in $vmss.VirtualMachineProfile.NetworkProfile.NetworkInterfaceConfigurations) {
+            $networkInterfaceConfiguration.NetworkSecurityGroup = $nsg.Id
+        }
+
+        # Saving VMSS
+        log -Message "[NSGCreation] Saving VMSS Named: $($vmss.Name)"
+        try {
+            $ErrorActionPreference = 'Stop'
+            Update-AzVmss -ResourceGroupName $vmssRg -VMScaleSetName $vmssName -VirtualMachineScaleSet $vmss > $null
+        }
+        catch {
+            $message = @"
+            [NSGCreation] An error occured while updating VMSS '$($vmss.name)' to associate the new NSG '$("NSG-"+$vmss.Name)'. TRAFFIC FROM LOAD BALANCER TO
+            BACKEND POOL MEMBERS WILL BE BLOCKED UNTIL AN NSG WITH AN ALLOW RULE IS CREATED! To recover, manually associate NSG '$("NSG-"+$vmss.Name)'
+            with the VM, VMSS, or subnet. Error: $_
+"@
+            log 'Error' $message
+            Exit
+        }
+
+        UpdateVmssInstances -vmss $vmss
+    }
+    log -Message "[NSGCreation] NSG Creation Completed"
+}
+
+Export-ModuleMember -Function NSGCreation
