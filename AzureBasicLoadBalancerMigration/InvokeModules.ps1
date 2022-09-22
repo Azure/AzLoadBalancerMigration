e246673b3df292d23c5c820c9267fd75934d9e17
welasco <victor@hepoca.com>
2022-09-22 10:44:33 -0500

    Cleanup Repo

--- AzureBasicLoadBalancerMigration/InvokeModules.ps1
+++ AzureBasicLoadBalancerMigration/InvokeModules.ps1
@@ -1,32 +1,12 @@
 Remove-Module AzureBasicLoadBalancerMigration -force
-Import-Module C:\Projects\VSProjects\VMSS-LoadBalander-MIgration\AzureBasicLoadBalancerMigration\AzureBasicLoadBalancerMigration.psd1 -Force
+Import-Module C:\Projects\VSProjects\AzLoadBalancerMigration\AzureBasicLoadBalancerMigration\AzureBasicLoadBalancerMigration.psd1 -Force
 
-# External
 #Start-AzBasicLoadBalancerMigration -ResourceGroupName basiclb -BasicLoadBalancerName basiclb-loadbalancer -StandardLoadBalancerName stdlb-loadbalancer
-# Start-AzBasicLoadBalancerMigration -ResourceGroupName basiclb -BasicLoadBalancerName basiclb-loadbalancer
-#Start-AzBasicLoadBalancerMigration -RestoreFromJsonFile State-BasicLB-LoadBalancer-basiclb-20220907T1752322840.json
 #Start-AzBasicLoadBalancerMigration -ResourceGroupName "rg-009-basic-lb-ext-basic-static-pip" -BasicLoadBalancerName "lb-basic-01"
+#Start-AzBasicLoadBalancerMigration -FailedMigrationRetryFilePathLB C:\Projects\VSProjects\AzLoadBalancerMigration\State_lb-basic-01_rg-018-vmss-roll-upgrade-policy_20220919T0955511084.json -FailedMigrationRetryFilePathVMSS C:\Projects\VSProjects\AzLoadBalancerMigration\VMSS_vmss-01_rg-018-vmss-roll-upgrade-policy_20220919T0955511084.json
 
-# Internal
-#Start-AzBasicLoadBalancerMigration -ResourceGroupName BasicLBInternal -BasicLoadBalancerName BasicLBInternal-LoadBalancer
-#Start-AzBasicLoadBalancerMigration -ResourceGroupName rg-013-vmss-multi-be-single-lb -BasicLoadBalancerName lb-basic-01
-# test different RG
-#Start-AzBasicLoadBalancerMigration -ResourceGroupName rg-018-vmss-roll-upgrade-policy -BasicLoadBalancerName lb-basic-01
-
-Start-AzBasicLoadBalancerMigration -FailedMigrationRetryFilePathLB C:\Projects\VSProjects\VMSS-LoadBalander-MIgration\State_lb-basic-01_rg-018-vmss-roll-upgrade-policy_20220919T0955511084.json -FailedMigrationRetryFilePathVMSS C:\Projects\VSProjects\VMSS-LoadBalander-MIgration\VMSS_vmss-01_rg-018-vmss-roll-upgrade-policy_20220919T0955511084.json
-
-# Test IPV6
-# Start-AzBasicLoadBalancerMigration -ResourceGroupName rg-012-basic-lb-ext-ipv6-fe -BasicLoadBalancerName lb-basi-c01
-
-# Test with piping the object
 #$lb = Get-AzLoadBalancer -ResourceGroupName "rg-009-basic-lb-ext-basic-static-pip" -Name "lb-basic-01"
 #$lb | Start-AzBasicLoadBalancerMigration
-
-# Testing passing an object
-#$lb = Get-AzLoadBalancer -ResourceGroupName "rg-009-basic-lb-ext-basic-static-pip" -Name "lb-basic-01"
-#Start-AzBasicLoadBalancerMigration -BasicLoadBalancer $lb
-
-#Start-AzBasicLoadBalancerMigration -ResourceGroupName basiclb2 -BasicLoadBalancerName basiclb2-loadbalancer -StandardLoadBalancerName stdlb2-loadbalancer
 
 ############################################
 # Build test environment
@@ -40,4 +20,3 @@ Start-AzBasicLoadBalancerMigration -FailedMigrationRetryFilePathLB C:\Projects\V
 # Cleanup
 # .\deploy.ps1 -Cleanup
 ############################################
-# Remove-AzResourceGroup -Name rg-018-vmss-roll-upgrade-policy -Force;.\deploy.ps1 -Location CentralUS -KeyVaultResourceGroupName rg-vmsstestingconfig -ScenarioNumber 018
