949858de3f07575c76eaa90a8331fff89b387f57
Matthew Bratschun <matthew.bratschun@microsoft.com>
2023-01-25 10:09:06 -0700

    consistent VMSS NSG and NAT casing

--- AzureBasicLoadBalancerUpgrade/modules/GetVMSSFromBasicLoadBalancer/GetVMSSFromBasicLoadBalancer.psd1
+++ AzureBasicLoadBalancerUpgrade/modules/GetVMSSFromBasicLoadBalancer/GetVMSSFromBasicLoadBalancer.psd1
@@ -9,7 +9,7 @@
 @{
 
 # Script module or binary module file associated with this manifest.
-RootModule = 'GetVMSSFromBasicLoadBalancer'
+RootModule = 'GetVmssFromBasicLoadBalancer'
 
 # Version number of this module.
 ModuleVersion = '0.1.1'
--- AzureBasicLoadBalancerUpgrade/modules/GetVMSSFromBasicLoadBalancer/GetVMSSFromBasicLoadBalancer.psm1
+++ AzureBasicLoadBalancerUpgrade/modules/GetVMSSFromBasicLoadBalancer/GetVMSSFromBasicLoadBalancer.psm1
@@ -1,29 +1,29 @@
 # Load Modules
 Import-Module ((Split-Path $PSScriptRoot -Parent) + "\Log\Log.psd1")
-function GetVMSSFromBasicLoadBalancer {
+function GetVmssFromBasicLoadBalancer {
     [CmdletBinding()]
     param (
         [Parameter(Mandatory = $True)][Microsoft.Azure.Commands.Network.Models.PSLoadBalancer] $BasicLoadBalancer
     )
-    log -Message "[GetVMSSFromBasicLoadBalancer] Initiating GetVMSSFromBasicLoadBalancer"
+    log -Message "[GetVmssFromBasicLoadBalancer] Initiating GetVmssFromBasicLoadBalancer"
 
     try {
         $ErrorActionPreference = 'Stop'
         $vmssId = $BasicLoadBalancer.BackendAddressPools.BackendIpConfigurations.id | Foreach-Object { ($_ -split '/virtualMachines/')[0].ToLower() } | Select-Object -Unique
 
-        log -Message "[GetVMSSFromBasicLoadBalancer] Getting VMSS object '$vmssId' from Azure"
+        log -Message "[GetVmssFromBasicLoadBalancer] Getting VMSS object '$vmssId' from Azure"
         $vmss = Get-AzResource -ResourceId $vmssId | Get-AzVmss
     }
     catch {
         $message = @"
-        [GetVMSSFromBasicLoadBalancer] An error occured when getting VMSS '$($vmss.Name)' in resource group '$($vmss.ResourceGroupName)'. To recover
+        [GetVmssFromBasicLoadBalancer] An error occured when getting VMSS '$($vmss.Name)' in resource group '$($vmss.ResourceGroupName)'. To recover
         address the following error, and try again specifying the -FailedMigrationRetryFilePath parameter and Basic Load Balancer backup
         State file located either in this directory or the directory specified with -RecoveryBackupPath. `nError message: $_
 "@
         log 'Error' $message -terminateOnError
     }
-    log -Message "[GetVMSSFromBasicLoadBalancer] VMSS loaded Name $($vmss.Name) from RG $($vmss.ResourceGroupName)"
+    log -Message "[GetVmssFromBasicLoadBalancer] VMSS loaded Name $($vmss.Name) from RG $($vmss.ResourceGroupName)"
     return , $vmss
 }
 
-Export-ModuleMember -Function GetVMSSFromBasicLoadBalancer
+Export-ModuleMember -Function GetVmssFromBasicLoadBalancer
