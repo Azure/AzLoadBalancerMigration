725d3ddf7c385c72a3b3a1ecd8b2c3d8467e3824
Matthew Bratschun <25390936+mbrat2005@users.noreply.github.com>
2023-08-10 12:00:27 -0600

    Fixes to failed migration recovery for VM scenarios (#70)
    * allow array in -resourceGroupSuffix for deploy and cleanup actions
    
    * vm restore fixes
    
    * version bump

--- AzureBasicLoadBalancerUpgrade/testEnvs/scripts/deploy.ps1
+++ AzureBasicLoadBalancerUpgrade/testEnvs/scripts/deploy.ps1
@@ -5,7 +5,7 @@ Param (
     [switch]$includeManualConfigScenarios,
     [switch]$Cleanup, # removes all test environments (in parallel)
     [switch]$RunMigration, # executes the migration module against all test environments (in parallel),
-    [parameter(Mandatory = $false)][string]$resourceGroupSuffix = ''
+    [parameter(Mandatory = $false)][string[]]$resourceGroupSuffix = @('')
 )
 
 $ErrorActionPreference = 'Stop'
@@ -39,18 +39,20 @@ Else {
 
 Write-Verbose "Deploying templates: $($filteredTemplates.Name)"
 
-# if '-Cleanup' switch is supplied, remove the resource groups and exit
-if ($Cleanup -and $null -ne $filteredTemplates) {
-    $jobs = @()
+Foreach ($resourceGroupSuffixI in $resourceGroupSuffix) {
+    # if '-Cleanup' switch is supplied, remove the resource groups and exit
+    if ($Cleanup -and $null -ne $filteredTemplates) {
+        $jobs = @()
 
-    $rgNamesToRemove = $filteredTemplates | ForEach-Object {  "rg-{0}{1}-{2}" -f $_.BaseName.split('-')[0],$resourceGroupSuffix,$_.BaseName.split('-',2)[1] }
-    $rgNamesToRemove | Foreach-Object {
-        "Removing Resource Group '$_'"
-        $jobs += $(Remove-AzResourceGroup -Name $_ -Force -AsJob)
+        $rgNamesToRemove = $filteredTemplates | ForEach-Object { "rg-{0}{1}-{2}" -f $_.BaseName.split('-')[0], $resourceGroupSuffixI, $_.BaseName.split('-', 2)[1] }
+        $rgNamesToRemove | Foreach-Object {
+            "Removing Resource Group '$_'"
+            $jobs += $(Remove-AzResourceGroup -Name $_ -Force -AsJob)
+        }
+
+        $jobs | Wait-Job | Receive-Job
+        return
     }
-
-    $jobs | Wait-Job | Receive-Job
-    return
 }
 
 # if -RunMigration switch is supplied, the VMSS Load Balancer migration modules is run against all environments
@@ -67,11 +69,11 @@ if ($RunMigration -and $null -ne $filteredTemplates) {
         Start-AzBasicLoadBalancerUpgrade -ResourceGroupName $RGName -BasicLoadBalancerName lb-basic-01 -StandardLoadBalancerName lb-standard-01 -Pre -Force
     }
 
-    $rgNamesToMigrate = $filteredTemplates | ForEach-Object {  "rg-{0}{1}-{2}" -f $_.BaseName.split('-')[0],$resourceGroupSuffix,$_.BaseName.split('-',2)[1] }
-    $scenarios = Get-AzResourceGroup | Where-Object {$_.ResourceGroupName -iin $rgNamesToMigrate}
+    $rgNamesToMigrate = $filteredTemplates | ForEach-Object { "rg-{0}{1}-{2}" -f $_.BaseName.split('-')[0], $resourceGroupSuffix, $_.BaseName.split('-', 2)[1] }
+    $scenarios = Get-AzResourceGroup | Where-Object { $_.ResourceGroupName -iin $rgNamesToMigrate }
 
     $jobPool = @()
-    foreach($rg in $scenarios){
+    foreach ($rg in $scenarios) {
 
         While (($activeJobs = ($jobPool | Where-Object { $_.State -eq 'Running' }).count) -gt 10) {
             Write-Host "Currently $activeJobs jobs running, waiting for less than 10"
@@ -83,8 +85,7 @@ if ($RunMigration -and $null -ne $filteredTemplates) {
 
     Write-Output ("Total Jobs Created: " + $scenarios.Count)
     Write-Output "-----------------------------"
-    while((Get-Job -State Running).count -ne 0)
-    {
+    while ((Get-Job -State Running).count -ne 0) {
         Write-Output ("Threads Running: " + (Get-Job -State Running).count)
         Start-Sleep -Seconds 5
     }
@@ -111,45 +112,46 @@ if ($RunMigration -and $null -ne $filteredTemplates) {
 
 # deploy scenarioset-
 $jobs = @()
-
-foreach ($template in $filteredTemplates) {
-    $rgTemplateName = "rg-{0}{1}-{2}" -f $template.BaseName.split('-')[0],$resourceGroupSuffix,$template.BaseName.split('-',2)[1]
-    if($template.FullName -like "*.bicep"){
-        $params = @{
-            Name                    = "vmss-lb-deployment-$((get-date).tofiletime())"
-            TemplateFile            = $template.FullName
-            TemplateParameterObject = @{
-                Location                  = $Location
-                ResourceGroupName         = $rgTemplateName
+ForEach ($resourceGroupSuffixI in $resourceGroupSuffix) {
+    foreach ($template in $filteredTemplates) {
+        $rgTemplateName = "rg-{0}{1}-{2}" -f $template.BaseName.split('-')[0], $resourceGroupSuffixI, $template.BaseName.split('-', 2)[1]
+        if ($template.FullName -like "*.bicep") {
+            $params = @{
+                Name                    = "vmss-lb-deployment-$((get-date).tofiletime())"
+                TemplateFile            = $template.FullName
+                TemplateParameterObject = @{
+                    Location          = $Location
+                    ResourceGroupName = $rgTemplateName
+                }
             }
+
+            $job = New-AzSubscriptionDeployment -Location $location @params -AsJob
+            $job.Name = "rg-$($template.BaseName)"
+            $jobs += $job
         }
 
-        $job = New-AzSubscriptionDeployment -Location $location @params -AsJob
-        $job.Name = "rg-$($template.BaseName)"
-        $jobs += $job
-    }
+        elseif ($template.Name -like "019*.json") {
 
-    elseif($template.Name -like "019*.json"){
-
-        $params = @{
-            Name                    = "vmss-lb-deployment-$((get-date).tofiletime())"
-            TemplateFile            = $template.FullName
-        }
-        $null = New-AzResourceGroup -Name $rgTemplateName -Location $Location -Force -ErrorAction SilentlyContinue
-        $jobs += New-AzResourceGroupDeployment -ResourceGroupName $rgTemplateName @params -AsJob
-    }
-
-    elseif($template.Name -like "*.json"){
-        $params = @{
-            Name                    = "vmss-lb-deployment-$((get-date).tofiletime())"
-            TemplateFile            = $template.FullName
-            TemplateParameterObject = @{
-                Location                  = $Location
-                ResourceGroupName         = $rgTemplateName
+            $params = @{
+                Name         = "vmss-lb-deployment-$((get-date).tofiletime())"
+                TemplateFile = $template.FullName
             }
+            $null = New-AzResourceGroup -Name $rgTemplateName -Location $Location -Force -ErrorAction SilentlyContinue
+            $jobs += New-AzResourceGroupDeployment -ResourceGroupName $rgTemplateName @params -AsJob
         }
-        $null = New-AzResourceGroup -Name $rgTemplateName -Location $Location -Force -ErrorAction SilentlyContinue
-        $jobs += New-AzResourceGroupDeployment -ResourceGroupName $rgTemplateName @params -AsJob
+
+        elseif ($template.Name -like "*.json") {
+            $params = @{
+                Name                    = "vmss-lb-deployment-$((get-date).tofiletime())"
+                TemplateFile            = $template.FullName
+                TemplateParameterObject = @{
+                    Location          = $Location
+                    ResourceGroupName = $rgTemplateName
+                }
+            }
+            $null = New-AzResourceGroup -Name $rgTemplateName -Location $Location -Force -ErrorAction SilentlyContinue
+            $jobs += New-AzResourceGroupDeployment -ResourceGroupName $rgTemplateName @params -AsJob
+        }
     }
 }
 
